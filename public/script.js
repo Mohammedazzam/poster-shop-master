acd9705285a3d8209f5264678e75ef6628d57064
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-01-14 02:33:34 +0200

    (20)Rendering the list of products

--- public/script.js
+++ public/script.js
@@ -1,7 +1,12 @@
 new Vue({
 	el: "#app",
 	data:{
-		total: 0
+		total: 0,
+		products:[
+			{title:"product 1 "},
+			{title:"product 2 "},
+			{title:"product 3 "}
+		]
 	},
 	methods: {
 		addToCart:function(){
