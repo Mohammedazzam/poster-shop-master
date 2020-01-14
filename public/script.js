c78b1d919456946c640ebf2c1c2eea0e0c6cd8ba
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-01-14 14:09:09 +0200

    (26)Displaying cart items

--- public/script.js
+++ public/script.js
@@ -1,23 +1,22 @@
 new Vue({
 	el: "#app",
-	data:{
+	data: {
 		total: 0,
-		products:[
-			{title:"product 1",id:1,price:9.99},
-			{title:"product 2",id:2,price:9.99},
-			{title:"product 3",id:3,price:9.99}
-		]
+		products: [
+			{ title: "Product 1", id: 1, price: 9.99 },
+			{ title: "Product 2", id: 2, price: 9.99 },
+			{ title: "Product 3", id: 3, price: 9.99 }
+		],
+		cart: []
 	},
-	cart:[
 
-	],
 	methods: {
-		addToCart:function(product){
+		addToCart: function(product) {
 			// this.total +=9.99;
 			// console.log(product.id)
-			this.total +=product.price;
+			this.total += product.price;
 			this.cart.push(product);
-
 		}
-	},
-})
+	}
+});
+
