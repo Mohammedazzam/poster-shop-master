8bef450792d10db519d91b3035013c3b2a0df5a7
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-01-14 02:37:04 +0200

    (21)Adding key to iterated products

--- public/script.js
+++ public/script.js
@@ -3,9 +3,9 @@ new Vue({
 	data:{
 		total: 0,
 		products:[
-			{title:"product 1 "},
-			{title:"product 2 "},
-			{title:"product 3 "}
+			{title:"product 1", id:1},
+			{title:"product 2",id:2},
+			{title:"product 3", id:3}
 		]
 	},
 	methods: {
