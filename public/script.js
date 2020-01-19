b6c2e31bfc102abf236c8bde3fd1adb00a941ded
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-01-19 17:53:43 +0200

    (49) How scroll load works

--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
+var LOAD_NUM = 4;
+
 new Vue({
 	el: "#app",
 	data: {
 		total: 0,
 		products: [],
 		cart: [],
-		search:"cat",
-		lastSearch:"",
-		loading:false
+		search: "cat",
+		lastSearch: "",
+		loading: false,
+		results: []
 	},
-
 	methods: {
 		addToCart: function(product) {
-			// this.total +=9.99;
-			// console.log(product.id)
 			this.total += product.price;
 			var found = false;
 			for (var i = 0; i < this.cart.length; i++) {
@@ -28,35 +28,26 @@ new Vue({
 					price: product.price,
 					qty: 1
 				});
-		}
-		},
-		inc:function(item){
-			// console.log('inc')
-			item.qty++;
-			this.total +=item.price
-		},
-		dec:function(item){
-			// console.log('dec')
-			item.qty--;
-			this.total -=item.price
-			if(item.qty <= 0){   //هذه خاصة حتى ما أرجع لما بعد الصفر
-				var i = this.cart.indexOf(item);//هذه خاصة حتى ما أرجع لما بعد الصفر
-				this.cart.splice(i,1);//هذه خاصة حتى ما أرجع لما بعد الصفر
 			}
 		},
+		inc: function(item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function(item) {
+			item.qty--;
+			this.total -= item.price;
+		},
 		onSubmit: function() {
-			// console.log("search")
-			this.products =[];
-			this.loading =true;
+			this.products = [];
+			this.loading = true;
 			var path = "/search?q=".concat(this.search);
 			this.$http.get(path)
 				.then(function(response) {
-					// console.log(response);
-					setTimeout(function(){
-						this.products = response.body;
-						this.lastSearch = this.search;
-						this.loading = false;
-					}.bind(this), 3000);
+					this.results = response.body;
+					this.products = response.body.slice(0, LOAD_NUM);
+					this.lastSearch = this.search;
+					this.loading = false;
 				});
 		}
 	},
@@ -65,10 +56,7 @@ new Vue({
 			return "$".concat(price.toFixed(2));
 		}
 	},
-	created:function(){
+	created: function() {
 		this.onSubmit();
 	}
 });
-
-
-
